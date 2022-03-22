36917926d26ed6717a8135e9c51a71072671b6b3
Don Strange <dj.strange15@gmail.com>
2022-03-23 09:37:13 +1300

    Minor Changes to Backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 server.get('/', (req,res) => {
     res.send("API is running")
-})
+});
 server.get('/api/articles/:id', (req, res) => {
     const article = articles.find((n) => n._id === req.params.id);
     res.send(article);
