7be802f7043dee44bce69430eada5a2b0ab61d5f
Don Strange <dj.strange15@gmail.com>
2022-03-23 08:31:00 +1300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () =>
     <div className="App">
       <h1>Welcome to CISE - the home of learning and fun</h1>
     </div>
-  );
-}
 
 export default App;
